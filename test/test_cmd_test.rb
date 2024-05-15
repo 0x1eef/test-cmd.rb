8095c8270c6e60828fd93cf7770625c01599c3d8
0x1eef <0x1eef@protonmail.com>
2024-05-15 19:16:52 -0300

    Update tests

--- test/test_cmd_test.rb
+++ test/test_cmd_test.rb
@@ -1,47 +1,51 @@
 require_relative "setup"
 
-class CmdTest < Test::Unit::TestCase
-  def test_ruby_stdout
-    assert_equal "42\n", cmd("ruby", "-e", "puts 42").stdout
+class Test::Cmd
+  class Test < Test::Unit::TestCase
+  end
+end
+
+class Test::Cmd
+  ##
+  # Test::Cmd#argv
+  class ARGVTest < Test
+    def test_ruby_argv
+      assert_equal "42\n", cmd("ruby")
+                             .argv("-e", "warn 42")
+                             .stderr
+    end
   end
 
-  def test_ruby_stderr
-    assert_equal "42\n", cmd("ruby", "-e", "warn 42").stderr
+  ##
+  # Test::Cmd#{exit_status, status, success?}
+  class ExitStatusTest < Test
+    def test_ruby_exit_status_success
+      assert_equal 0, cmd("ruby", "-e", "exit 0").exit_status
+    end
+
+    def test_ruby_exit_status_failure
+      assert_equal 1, cmd("ruby", "-e", "exit 1").exit_status
+    end
+
+    def test_ruby_exit_status_predicates
+      assert_equal true, cmd("ruby", "-e", "exit 0").status.success?
+      assert_equal true, cmd("ruby", "-e", "exit 0").success?
+    end
   end
 
-  def test_ruby_success_exit_status
-    assert_equal 0, cmd("ruby", "-e", "exit 0").exit_status
-  end
+  ##
+  # Test::Cmd#{stdout,stderr}
+  class OutputTest < Test
+    def test_ruby_stdout
+      assert_equal "42\n", cmd("ruby", "-e", "puts 42").stdout
+    end
 
-  def test_ruby_failure_exit_status
-    assert_equal 1, cmd("ruby", "-e", "exit 1").exit_status
-  end
+    def test_ruby_stderr
+      assert_equal "42\n", cmd("ruby", "-e", "warn 42").stderr
+    end
 
-  def test_ruby_success_status
-    assert_equal true, cmd("ruby", "-e", "exit 0").status.success?
-    assert_equal true, cmd("ruby", "-e", "exit 0").success?
-  end
-
-  def test_ruby_success_callback
-    call_ok, call_fail = [false, false]
-    cmd("ruby", "-e", "exit 0")
-      .success { call_ok = true }
-      .failure { call_fail = true }
-    assert_equal true, call_ok
-    assert_equal false, call_fail
-  end
-
-  def test_ruby_failure_callback
-    call_ok, call_fail = [false, false]
-    cmd("ruby", "-e", "exit 1")
-      .success { call_ok = true }
-      .failure { call_fail = true }
-    assert_equal true, call_fail
-    assert_equal false, call_ok
-  end
-
-  def test_stdout_with_fork
-    code = <<-CODE.each_line.map { _1.chomp.strip }.join(";")
+    def test_ruby_stdout_fork
+      code = <<-CODE.each_line.map { _1.chomp.strip }.join(";")
       $stdout.sync = true
       fork do
         sleep(1)
@@ -50,18 +54,52 @@ class CmdTest < Test::Unit::TestCase
       puts "foo"
       Process.wait
     CODE
-    assert_equal "foo\nbar\n", cmd("ruby", "-e", code).stdout
+      assert_equal "foo\nbar\n", cmd("ruby", "-e", code).stdout
+    end
   end
 
-  def test_cmd_with_argv
-    assert_equal "42\n", cmd("ruby")
-                           .argv("-e", "warn 42")
-                           .stderr
+  ##
+  # Test::Cmd#{success, failure}
+  class CallbackTest < Test
+    def test_ruby_success_callback
+      call_ok, call_fail = [false, false]
+      cmd("ruby", "-e", "exit 0")
+        .success { call_ok = true }
+        .failure { call_fail = true }
+      assert_equal true, call_ok
+      assert_equal false, call_fail
+    end
+
+    def test_ruby_failure_callback
+      call_ok, call_fail = [false, false]
+      cmd("ruby", "-e", "exit 1")
+        .success { call_ok = true }
+        .failure { call_fail = true }
+      assert_equal true, call_fail
+      assert_equal false, call_ok
+    end
   end
 
-  def test_io_closed_after_spawn
-    result = cmd("ruby", "-e", "puts 42").spawn
-    assert_equal true, result.send(:out_io).closed?
-    assert_equal true, result.send(:err_io).closed?
+  ##
+  # Test::Cmd#spawn
+  class SpawnTest < Test
+    def test_io_closed_after_spawn
+      %i[out_io err_io].each do |io|
+        assert_equal true, command.send(io).closed?
+      end
+    end
+
+    def test_io_unlink_after_spawn
+      %i[out_io err_io].each do |io|
+        path = command.send(io).__getobj__.path
+        assert_equal false, File.exist?(path)
+      end
+    end
+
+    private
+
+    def command
+      cmd("ruby", "-e", "puts 42").spawn
+    end
   end
 end
